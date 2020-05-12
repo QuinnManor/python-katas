e1587c2a0a075869163231cbab78c0ca4f77f9f2
QuinnManor <matthewq1428@gmail.com>
2020-05-11 21:25:34 -0400

    Cleaning up files

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,4 @@
 def fizzbuzz(num):
-    response = ''
     if (num % 3 == 0) and (num % 5 == 0):
         response = 'fizzbuzz'
     elif num % 3 == 0:
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,11 +4,14 @@ from fizzbuzz import fizzbuzz
 def test_fizz():
     assert fizzbuzz(3) == 'fizz'
 
+
 def test_buzz():
     assert fizzbuzz(5) == 'buzz'
 
+
 def test_fizzbuzz():
     assert fizzbuzz(15) == 'fizzbuzz'
+
 
 def test_number():
     assert fizzbuzz(1) == 1

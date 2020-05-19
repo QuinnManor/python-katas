a2e5b10d48e8f65c04976e7d9c7f889ed7e4a765
QuinnManor <matthewq1428@gmail.com>
2020-05-19 13:08:51 -0400

    Calculates earliest start time

--- babysitter.py
+++ babysitter.py
@@ -1,2 +1,4 @@
 def calculate_hours(start_time, end_time):
+    if start_time < 5:
+        start_time = 5
     return end_time - start_time
--- test_babysitter.py
+++ test_babysitter.py
@@ -5,3 +5,9 @@ def test_calculate_hours():
     start_time = 5
     end_time = 6
     assert calculate_hours(start_time, end_time) == 1
+
+
+def test_earliest_start_time():
+    start_time = 4
+    end_time = 6
+    assert calculate_hours(start_time, end_time) == 1

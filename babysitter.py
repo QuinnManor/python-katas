0cc3ddf33a39135527fbb053ca9dbb6f910fac39
QuinnManor <matthewq1428@gmail.com>
2020-05-19 13:13:53 -0400

    Can calculate latest end time. Processes time of day

--- babysitter.py
+++ babysitter.py
@@ -1,4 +1,8 @@
-def calculate_hours(start_time, end_time):
-    if start_time < 5:
+def calculate_hours(start_time, end_time, time_of_day='PM'):
+    if start_time < 5 and time_of_day == 'PM':
         start_time = 5
+
+    if end_time > 4 and time_of_day == 'AM':
+        end_time = 4
+
     return end_time - start_time
--- test_babysitter.py
+++ test_babysitter.py
@@ -11,3 +11,10 @@ def test_earliest_start_time():
     start_time = 4
     end_time = 6
     assert calculate_hours(start_time, end_time) == 1
+
+
+def test_latest_end_time():
+    start_time = 3
+    end_time = 5
+    time_of_day = 'AM'
+    assert calculate_hours(start_time, end_time, time_of_day) == 1
